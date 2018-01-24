85f1b9aa6089a738c92646d350fd986543e67bf2
alexander <axdowney@cedarville.edu>
2018-01-24 09:51:01 -0600

    * Added replace function in StringUtils

--- StringUtils.cpp
+++ StringUtils.cpp
@@ -1,23 +1,44 @@
-/* Copyright (C) 2017 Alexander Downey */
+/* Copyright (C) 2017-2018 Alexander Downey */
 #include "StringUtils.h"
+
+#include <string>
 
 using namespace std;
 
 //Copied from https://stackoverflow.com/questions/289347/using-strtok-with-a-stdstring
-void StringUtils::split(const string& str, const string& delim, vector<string>& parts) {
-    size_t start, end = 0;
+void StringUtils::split(const string& str, const string& delim, vector<string>& parts, bool bExact, bool bIncludeEmpty) {
+    if (delim.empty()) {
+        if (!str.empty() || bIncludeEmpty) {
+            parts.push_back(str);
+        }
+        return;
+    }
+    size_t start = 0, end = 0;
     while (end < str.size()) {
-	start = end;
-	while (start < str.size() && (delim.find(str[start]) != string::npos)) {
-	    start++;  // skip initial whitespace
-	}
-	end = start;
-	while (end < str.size() && (delim.find(str[end]) == string::npos)) {
-	    end++; // skip to end of word
-	}
-	if (end-start != 0) {  // just ignore zero-length strings.
-	    parts.push_back(string(str, start, end-start));
-	}
+        if (!bExact) {
+            start = end;
+            while (start < str.size() && (delim.find(str[start]) != string::npos)) {
+                start++;  // skip initial whitespace
+            }
+            end = start;
+            while (end < str.size() && (delim.find(str[end]) == string::npos)) {
+                end++; // skip to end of word
+            }
+            if (end-start != 0 || bIncludeEmpty) {  // just ignore zero-length strings.
+                parts.push_back(string(str, start, end-start));
+            }
+        } else {
+            end = str.find(delim, start);
+            if (start != end || bIncludeEmpty) {
+                parts.push_back(str.substr(start, end - start));
+            }
+
+            if (end == string::npos) {
+                break;
+            } else {
+                start = end + delim.size();
+            }
+        }
     }
 }
 
@@ -25,7 +46,7 @@ void StringUtils::combine(std::string& str, const std::string& delim, const std:
     str = "";
     for (size_t i = 0; i < parts.size(); ++i) {
         if (bIncludeEmpty || !parts[i].empty()) {
-            if (!str.empty()) {
+            if (!str.empty() || bIncludeEmpty && i > 0) {
                 str += delim;
             }
 
@@ -38,4 +59,12 @@ std::string StringUtils::getExt(const std::string sFile) {
     vector<string> parts;
     split(sFile, ".", parts);
     return parts.size() > 1 ? parts.back() : std::string();
+}
+
+std::string StringUtils::replace(const std::string& str, const std::string& sReplace, std::string sReplaceWith) {
+    std::vector<std::string> vec;
+    std::string sRet;
+    split(str, sReplace, vec, true, true);
+    combine(sRet, sReplaceWith, vec, true);
+    return sRet;
 }
--- StringUtils.h
+++ StringUtils.h
@@ -1,10 +1,11 @@
-/* Copyright (C) 2017 Alexander Downey */
+/* Copyright (C) 2017-2018 Alexander Downey */
 #include <string>
 #include <vector>
 
 class StringUtils {
     public:
-        static void split(const std::string& str, const std::string& delim, std::vector<std::string>& parts);
+        static void split(const std::string& str, const std::string& delim, std::vector<std::string>& parts, bool bExact = false, bool bIncludeEmpty = false);
         static void combine(std::string& str, const std::string& delim, const std::vector<std::string>& parts, bool bIncludeEmpty = false);
         static std::string getExt(const std::string sFile);
+        static std::string replace(const std::string& str, const std::string& sReplace, std::string sReplaceWith);
 };
